f83804c2d07fea7ae84f40e3d756b44c2c8e0d83
ajharul <mdajharulislam327@gmail.com>
2025-09-11 17:48:14 +0600

    Add a back button

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,25 +1,37 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const Product = () => {
-  const {id} = useParams();
-  const[store, setStore] = useState('')
-   const fetchData = async ()=>{
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
-    setStore(response.data)
-   }
- useEffect(()=>{
-  fetchData()
- },[])
+  const { id } = useParams();
+  const [store, setStore] = useState("");
+  const navigate = useNavigate();
+  const fetchData = async () => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    setStore(response.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1>product {id}</h1>
-      <h1>product: {store.username}</h1>
-      <h1>address: {store?.address?.city}</h1>
+    <div className="flex justify-center items-center m-10 ">
+      <div className=" w-fit h-fit p-4 p-10  border border-amber-500 rounded-2xl ">
+        <h1>product {id}</h1>
+        <h1>product: {store.username}</h1>
+        <h1>address: {store?.address?.city}</h1>
+        <h1>Zipcode: {store?.address?.zipcode}</h1>
+        <button
+          onClick={() => navigate("/")}
+          className="border border-amber-200 bg-amber-950 rounded-md text-white p-2 text-xl cursor-pointer"
+        >
+          Go Back
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Product;
