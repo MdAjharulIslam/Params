f42eaedf014390511e411e0033321981892f378a
ajharul <mdajharulislam327@gmail.com>
2025-09-12 16:32:13 +0600

    add HoC

---
+++ HOC.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+
+//normal components
+const Normal = ()=>{
+    return (
+        <div>
+            <h1>hello normal</h1>
+        </div>
+    )
+}
+
+//HOC
+const App = (WropedComponent)=>{
+    return function add(){
+
+        return <WropedComponent  />
+    }
+}
+
+const Hoc = App(Normal);
