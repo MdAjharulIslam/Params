85af2335f95208fa6bd09a18094bef4cce6ae4e0
ajharul <mdajharulislam327@gmail.com>
2025-09-12 15:38:54 +0600

    re practice lazy loading

--- LazyLoading.jsx
+++ LazyLoading.jsx
@@ -1,13 +1,32 @@
-import React, { lazy, Suspense } from 'react'
+// import React, { lazy, Suspense } from 'react'
 
-const Product = lazy(()=> import('./Product'))
+// const Product = lazy(()=> import('./Product'))
 
-const LazyLoading = () => {
-  return (
+// const LazyLoading = () => {
+//   return (
+//     <div>
+//       <Suspense  fallback= {<h1>Loading...</h1>}>
+//        <Product/>
+//       </Suspense>
+//     </div>
+//   )
+// }
+
+// export default LazyLoading;
+
+
+import React from 'react';
+import {lazy, Suspence} from 'react'
+
+const LazyLoading = ()=>{
+const Product = lazy(()=> import('./src/Product'));
+
+  return(
     <div>
-      <Suspense  fallback= {<h1>Loading...</h1>}>
-       <Product/>
-      </Suspense>
+    <Suspence  fallback={<h1>Loading...</h1> }>
+      <Product/>
+    </Suspence>
+    
     </div>
   )
 }
