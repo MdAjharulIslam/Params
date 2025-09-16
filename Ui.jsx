0c9e761ba9acc3fbf564c0bd6243d3f6b345a502
ajharul <mdajharulislam327@gmail.com>
2025-09-17 00:07:10 +0600

    currection space

--- Ui.jsx
+++ Ui.jsx
@@ -40,7 +40,7 @@ const Ui = () => {
   </div>
   <div>
      <div className='flex justify-center items-center h-screen'>
-      <div className='animate-spin rounded-full h-16 w-16 border-4 border-t-white border-gray-700 '></div>
+      <div className='animate-spin rounded-full h-16 w-16 border-4 border-t-white border-gray-700'></div>
     </div>
   </div>
     </div>
